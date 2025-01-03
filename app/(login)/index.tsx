2df23cf462aa98f7163e79733295c02fdc1fec8b
pulkit <maheshwariloya@gmail.com>
2025-01-03 13:05:14 +0530

    Added firebase auth

--- app/(login)/index.tsx
+++ app/(login)/index.tsx
@@ -1,8 +1,18 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { TextInput } from 'react-native-gesture-handler'
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 export default function index() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBc24lPTje7pq_twb5Ggf2o4fyexu86nGw",
+        authDomain: "socialmediaapp-rn.firebaseapp.com",
+        projectId: "socialmediaapp-rn",
+        storageBucket: "socialmediaapp-rn.firebasestorage.app",
+        messagingSenderId: "362433250393",
+        appId: "1:362433250393:web:9de83259191b1f947f5d48",
+        measurementId: "G-BQZQMPPWSX"
+      };
     const [email,setEmail] = useState("");
     const [password, setPassword] = useState("");
 
